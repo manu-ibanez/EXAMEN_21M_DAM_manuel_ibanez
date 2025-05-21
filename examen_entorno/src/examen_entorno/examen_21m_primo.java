42f1fcdcf1ecc5425ace02c76b46ce4e58437b9a
Manuel <Manuel@DESKTOP-manu>
2025-05-21 11:41:37 +0200

    primer commit en rama1

--- examen_entorno/src/examen_entorno/examen_21m_primo.java
+++ examen_entorno/src/examen_entorno/examen_21m_primo.java
@@ -2,6 +2,8 @@ package examen_entorno;
 
 import java.util.Scanner;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class examen_21m_primo {
 
 	public static void main(String[] args) {
@@ -11,5 +13,13 @@ public class examen_21m_primo {
 				+ "Introduce un número para averiguar si es primo o no:");
 		int numIntroducido = Integer.parseInt(ent.nextLine());
 
+		
+		System.out.println("El número no es primo \n"
+				+ "Los divisores del número son: \n"
+				+ "Ejemplo: El 4 no es primo\n"
+				+ "	ejemplo: Los divisores del 4 son: 1,2, y 4");
+		
+		
+		
 	}
 }
