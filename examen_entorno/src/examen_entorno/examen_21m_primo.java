36c7cd027c5bc595e527d37569500721979dd431
Manuel <Manuel@DESKTOP-manu>
2025-05-21 11:27:36 +0200

    creacion de clase, pedir por teclado y primer commit

---
+++ examen_entorno/src/examen_entorno/examen_21m_primo.java
@@ -0,0 +1,15 @@
+package examen_entorno;
+
+import java.util.Scanner;
+
+public class examen_21m_primo {
+
+	public static void main(String[] args) {
+		Scanner ent = new Scanner(System.in);
+
+		System.out.println("Buenos días Ceinmark, soy Marta Gómez" + "\n"
+				+ "Introduce un número para averiguar si es primo o no:");
+		int numIntroducido = Integer.parseInt(ent.nextLine());
+
+	}
+}
